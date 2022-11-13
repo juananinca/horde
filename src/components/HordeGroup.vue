f3d06b4ce856520aaf51c8d85dbf152105f5ae48
Juan Antonio Lopez Muntaner <juananinca@gmail.com>
2022-11-13 21:16:37 +0100

    changed select options

--- src/components/HordeGroup.vue
+++ src/components/HordeGroup.vue
@@ -11,16 +11,22 @@ export default defineComponent({
       beastOptions: horde,
       beastOptionsValue: new Creature("", []),
       groupDamage: "",
-      numberOfCreatures: "",
-      enemyCa: 10,
+      numberOfCreatures: -1,
+      enemyCa: -1,
       uuid: uuidv4(),
-      numberOfCreaturesOptions: [1, 2, 3, 4, 5, 6, 7, 8],
-      enemyCA: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, ]
+      numberOfCreaturesOptions: [
+        {value: -1, text: "Numero de Criaturas", disabled: true},
+        1, 2, 3, 4, 5, 6, 7, 8
+      ],
+      enemyCAOptions: [
+        {value: -1, text: "CA del enemigo", disabled: true},
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25
+      ]
     }
   },
   methods: {
     rollGroup() {
-      if (!this.numberOfCreatures) {
+      if (!this.numberOfCreatures || !this.enemyCa) {
         alert("Alguno de los campos obligatorios esta vacio!!")
         return
       }
@@ -44,22 +50,20 @@ export default defineComponent({
           id="horde-group-beast-selector"
           class="mb-2 mr-sm-2 mb-sm-0"
           v-model="beastOptionsValue">
-            <option :value="null" disabled>-- Please select an option --</option>
+            <option :value="beastOptionsValue" disabled>Selecciona una bestia</option>
             <option v-for="option in beastOptions" :value="option">
               {{ option.name }}
             </option>
           </b-form-select></b-col>
       </b-form-row>
       <b-form-row>
-        <!-- <b-col><b-form-input v-model="numberOfCreatures" placeholder="Numero de criaturas" /></b-col> -->
-        <b-col><b-form-select class="mb-2 mr-sm-2 mb-sm-0" v-model="numberOfCreatures" :options="[1, 2, 3, 4, 5, 6, 7, 8]"></b-form-select></b-col>
-        <b-col><b-form-input v-model="enemyCa" placeholder="CA del enemigo" pattern="\d*"/></b-col>
-        <!-- <b-col><b-form-select class="mb-2 mr-sm-2 mb-sm-0" v-model="enemyCa" :options="[1, 2, 3, 4, 5, 6, 7, 8]"></b-form-select></b-col> -->
+        <b-col><b-form-select class="mb-2 mr-sm-2 mb-sm-0" v-model="numberOfCreatures" :options="numberOfCreaturesOptions" placeholder="Numero de Criaturas"></b-form-select></b-col>
+        <b-col><b-form-select class="mb-2 mr-sm-2 mb-sm-0" v-model="enemyCa" :options="enemyCAOptions" placeholder="CA del enemigo"></b-form-select></b-col>
       </b-form-row>
       <hr/>
       <b-form-row>
         <b-col><b-button @click="rollGroup">Roll!</b-button></b-col>
-        <b-col>{{groupDamage}}</b-col>
+        <b-col><b-button v-if="groupDamage !== ''" variant="primary">{{groupDamage}}</b-button></b-col>
       </b-form-row>
     </b-card>
   </div>
